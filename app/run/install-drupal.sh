8ba652f0c9751d6d2312cae058f031a0227b5217
Thorsten Krug <email@donkeymedia.eu>
2018-05-21 17:50:27 -0400

    Refactoring based on Docker PHP library.

--- app/run/install-drupal.sh
+++ app/run/install-drupal.sh
@@ -6,9 +6,12 @@ rm -rf  /var/www/drupal/drush
 
 # Install Drupal
 
+# echo "Environment variables"
+# env
+
 echo "Drupal site install with Drush"
-echo "/var/www/.composer/vendor/bin/drush site-install standard -y --root=/var/www/drupal/web --db-url='mysql://root:${MYSQL_ROOT_PASSWORD}@${MYSQL_HOSTNAME}:${MYSQL_PORT}/drupal' --account-name='${DRUPAL_ACCOUNT_NAME}' --account-pass='${DRUPAL_ACCOUNT_PASS}' --site-name='${DRUPAL_SITE_NAME}' --account-mail='${DRUPAL_ACCOUNT_MAIL}' --site-mail='${DRUPAL_SITE_MAIL}' --notify='global'"
-/var/www/.composer/vendor/bin/drush site-install standard -y --root=/var/www/drupal/web --db-url="mysql://root:${MYSQL_ROOT_PASSWORD}@${MYSQL_HOSTNAME}:${MYSQL_PORT}/drupal" --account-name="${DRUPAL_ACCOUNT_NAME}" --account-pass="${DRUPAL_ACCOUNT_PASS}" --site-name="${DRUPAL_SITE_NAME}" --account-mail="${DRUPAL_ACCOUNT_MAIL}" --site-mail="${DRUPAL_SITE_MAIL}" --notify="global"
+echo "/var/www/.composer/vendor/bin/drush site-install standard -y --root=/var/www/drupal/web --db-url=\"mysql://root:${MYSQL_ROOT_PASSWORD}@${MYSQL_HOSTNAME}:${MYSQL_PORT}/${MYSQL_DATABASE}\" --account-name=\"${DRUPAL_ACCOUNT_NAME}\" --account-pass=\"${DRUPAL_ACCOUNT_PASS}\" --site-name=\"${DRUPAL_SITE_NAME}\" --account-mail=\"${DRUPAL_ACCOUNT_MAIL}\" --site-mail=\"${DRUPAL_SITE_MAIL}\" --notify=\"global\""
+/var/www/.composer/vendor/bin/drush site-install standard -y --root=/var/www/drupal/web --db-url="mysql://root:${MYSQL_ROOT_PASSWORD}@${MYSQL_HOSTNAME}:${MYSQL_PORT}/${MYSQL_DATABASE}" --account-name="${DRUPAL_ACCOUNT_NAME}" --account-pass="${DRUPAL_ACCOUNT_PASS}" --site-name="${DRUPAL_SITE_NAME}" --account-mail="${DRUPAL_ACCOUNT_MAIL}" --site-mail="${DRUPAL_SITE_MAIL}" --notify="global"
 
 cd /var/www/drupal/web && /var/www/.composer/vendor/bin/drush cr
 
